2ee729ff023c8fde8a1bdb7b68243039c55fd63e
maliang111 <ml85407130@126.com>
2023-01-24 21:12:26 +0800

    添加段落

--- word-add-in-demo/src/taskpane/taskpane.js
+++ word-add-in-demo/src/taskpane/taskpane.js
@@ -7,24 +7,29 @@
 
 Office.onReady((info) => {
   if (info.host === Office.HostType.Word) {
-    document.getElementById("sideload-msg").style.display = "none";
-    document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
+    // Determine if the user's version of Office supports all the Office.js APIs that are used in the tutorial.
+    if (!Office.context.requirements.isSetSupported('WordApi', '1.3')) {
+      console.log('Sorry. The tutorial add-in uses Word.js APIs that are not available in your version of Office.');
+    }
+
+  // Assign event handlers and other initialization logic.
+    document.getElementById("insert-paragraph").onclick = insertParagraph;
   }
 });
 
-export async function run() {
-  return Word.run(async (context) => {
-    /**
-     * Insert your Word code here
-     */
+async function insertParagraph() {
+  await Word.run(async (context) => {
 
-    // insert a paragraph at the end of the document.
-    const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
-
-    // change the paragraph color to blue.
-    paragraph.font.color = "blue";
-
+    // TODO1: Queue commands to insert a paragraph into the document.
+    const docBody = context.document.body;
+    docBody.insertParagraph("Office has several versions, including Office 2016, Microsoft 365 subscription, and Office on the web.",
+        "Start");
     await context.sync();
-  });
+  })
+      .catch(function (error) {
+        console.log("Error: " + error);
+        if (error instanceof OfficeExtension.Error) {
+          console.log("Debug info: " + JSON.stringify(error.debugInfo));
+        }
+      });
 }
